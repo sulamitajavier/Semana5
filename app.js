0ee9eb34a928711c9e7c27ae29017d6a49dd48fc
sulamitajavier <sulamitajavier@gmail.com>
2017-10-10 17:25:15 -0400

    Semana 5
     status box

---
+++ app.js
@@ -0,0 +1,38 @@
+var main = function() {
+  $('.btn').click(function() {
+    var post = $('.status-box').val();
+    $('<li>').text(post).prependTo('.posts');
+    $('.status-box').val('');
+    $('.counter').text('140');
+    $('.btn').addClass('disabled'); 
+  });
+  
+  $('.status-box').keyup(function() {
+    var postLength = $(this).val().length;
+    var charactersLeft = 140 - postLength;
+    $('.counter').text(charactersLeft);
+  
+    if(charactersLeft < 0) {
+      $('.btn').addClass('disabled'); 
+    }
+    else if(charactersLeft == 140) {
+      $('.btn').addClass('disabled');
+    }
+    else {
+      $('.btn').removeClass('disabled');
+    }
+  });
+  
+  $('.btn').addClass('disabled');
+}
+
+$(document).ready(main);
+
+window.onload = choosePic;
+
+var myPix = new Array("img/7.jpg", "img/tres.jpg","img/6.jpg");
+
+function choosePic() {
+  randomNum = Math.floor((Math.random() * myPix.length));
+  document.getElementById("myPicture").src = myPix[randomNum];
+}
